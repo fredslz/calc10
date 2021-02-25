6fc0cd7dc50122873a5e218edefa5c950c05df27
fredslz <fredslz@gmail.com>
2021-02-24 21:37:27 -0300

    Update test_pytestes.py

--- test_pytestes.py
+++ test_pytestes.py
@@ -2,6 +2,6 @@ import pytest
 from app import soma
 
 
-@pytest.mark.parametrize('a,b,expected', [(5, 4, 9)])
+@pytest.mark.parametrize('a,b,expected', [(5, 4, 9), (-5, 4, -1), (-5, -4, -9), (-5.4, -4.2, -9.6)])
 def test_soma(a,b, expected):
     assert soma(a,b) == expected
